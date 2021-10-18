6fa6796b7464f0d6fd480a2a9108858ddc02ae35
youngmikec <michaelozor15@gmail.com>
2021-10-18 22:56:15 +0100

    first commit

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,13 +1,43 @@
-function seed() {}
+function seed(...args) {
+  return args;
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  return (x === j) && (y === k) ? true : false;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return this.some((e) => same(e, cell));
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  return contains.call(state, cell) ? '\u25A3' : '\u25A2';
+};
 
-const corners = (state = []) => {};
+const corners = (state = []) => {
+  if(state.length === 0){
+    return {
+      topRight: [0,0],
+      bottomLeft: [0,0]
+    }
+
+  }
+  
+  const xs = state.map(([x, _]) => x);
+  const ys = state.map(([_, y]) => y);
+
+  return {
+    topRight: [
+      Math.max(...xs), 
+      Math.max(...ys)
+    ],
+    bottomLeft: [
+      Math.min(...xs),
+      Math.min(...ys)
+    ]
+  }
+};
 
 const printCells = (state) => {};
 
